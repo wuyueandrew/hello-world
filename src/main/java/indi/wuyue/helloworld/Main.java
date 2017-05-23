42a3e9776f3593c43f67e35cc8f48890dc9bd8a3
wuyue <wuyue_andrew@outlook.com>
2017-05-24 01:58:28 +0800

    first commit

---
+++ src/main/java/indi/wuyue/helloworld/Main.java
@@ -0,0 +1,10 @@
+package indi.wuyue.helloworld;
+
+/**
+ * Created by wuyue on 17/5/24.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
